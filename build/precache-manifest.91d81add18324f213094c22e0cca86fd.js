f9bdccd4fd6c47aa24c1fd23fb5b8c655099dd34
Tural Omarov <omarov.t@gmail.com>
2020-08-23 19:21:13 +0400

    update-build-1

---
+++ build/precache-manifest.91d81add18324f213094c22e0cca86fd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569796f2270b26854aff9a5a8a83a4e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70fe179dfe285d585333",
+    "url": "/static/css/2.fd971cca.chunk.css"
+  },
+  {
+    "revision": "e95ad02656c9e4d0bcea",
+    "url": "/static/css/main.65ce2c01.chunk.css"
+  },
+  {
+    "revision": "70fe179dfe285d585333",
+    "url": "/static/js/2.78f48d29.chunk.js"
+  },
+  {
+    "revision": "0d10b9de47c8023f7fd9d830b32d5242",
+    "url": "/static/js/2.78f48d29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e95ad02656c9e4d0bcea",
+    "url": "/static/js/main.c127def3.chunk.js"
+  },
+  {
+    "revision": "fb645d62c19d0d937e06",
+    "url": "/static/js/runtime-main.99058073.js"
+  }
+]);
